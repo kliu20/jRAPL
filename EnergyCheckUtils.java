748da2919e6e9ea9820ed29781393e80f859a6e8
Gustavo Pinto <gustavohenrique.86@gmail.com>
2015-01-17 17:28:04 -0300

    improving readme

--- EnergyCheckUtils.java
+++ EnergyCheckUtils.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Field;
 public class EnergyCheckUtils {
 	public native static int scale(int freq);
 	public native static int[] freqAvailable();
-	
+
 	public native static double[] GetPackagePowerSpec();
 	public native static double[] GetDramPowerSpec();
 	public native static void SetPackagePowerLimit(int socketId, int level, double costomPower);
@@ -14,33 +14,30 @@ public class EnergyCheckUtils {
 	public native static String EnergyStatCheck();
 	public native static void ProfileDealloc();
 	public native static void SetPowerLimit(int ENABLE);
-	public static int wraparoundValue;	
-	
+	public static int wraparoundValue;
+
 	public static int socketNum;
 	static {
-		System.setProperty("java.library.path",
-				System.getProperty("user.dir"));
+		System.setProperty("java.library.path", System.getProperty("user.dir"));
 //				System.out.print(System.getProperty("user.dir"));
 		try {
-			Field fieldSysPath = ClassLoader.class
-					.getDeclaredField("sys_paths");
+			Field fieldSysPath = ClassLoader.class.getDeclaredField("sys_paths");
 			fieldSysPath.setAccessible(true);
 			fieldSysPath.set(null, null);
-		} catch (Exception e) {
+		} catch (Exception e) { }
 
-		}
 		System.loadLibrary("CPUScaler");
 		wraparoundValue = ProfileInit();
 		socketNum = GetSocketNum();
 	}
-	
+
 	/**
 	 * @return an array of current energy information.
 	 * The first entry is: Dram/uncore gpu energy(depends on the cpu architecture.
 	 * The second entry is: CPU energy
 	 * The third entry is: Package energy
 	 */
-	
+
 	public static double[] getEnergyStats() {
 		socketNum = GetSocketNum();
 		String EnergyInfo = EnergyStatCheck();
@@ -48,20 +45,20 @@ public class EnergyCheckUtils {
 		if(socketNum == 1) {
 			double[] stats = new double[3];
 			String[] energy = EnergyInfo.split("#");
-			
+
 			stats[0] = Double.parseDouble(energy[0]);
 			stats[1] = Double.parseDouble(energy[1]);
 			stats[2] = Double.parseDouble(energy[2]);
-			
+
 			return stats;
-		
+
 		} else {
 		/*Multiple sockets*/
 			String[] perSockEner = EnergyInfo.split("@");
 			double[] stats = new double[3*socketNum];
 			int count = 0;
 
-			
+
 			for(int i = 0; i < perSockEner.length; i++) {
 				String[] energy = perSockEner[i].split("#");
 				for(int j = 0; j < energy.length; j++) {
@@ -73,7 +70,7 @@ public class EnergyCheckUtils {
 		}
 
 	}
-	
+
 	public static void main(String[] args) {
 
 		double[] before = getEnergyStats();
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Introduction:
+Introduction
 --------------------------------------------------------------
 
 For a general explanation, check our [webpage](http://kliu20.github.io/jRAPL/).
@@ -13,7 +13,7 @@ You need check which one is your CPU architecture before you use jRAPL.
 
 For more details about RAPL, check the main [reference](https://software.intel.com/en-us/articles/intel-power-governor)
 
-Pre-requisites:
+Pre-requisites
 --------------------------------------------------------------
 
 This library uses the kernel `msr` module. To use, type
@@ -36,22 +36,18 @@ How to use
 
 Everytime you need root/sudo access to run the Java code in order to access RAPL MSR.
 
-`EnergyCheckUtils.java` is a sample drive code.
-
-It includes the following methods, and it needs to be declared like the following before usage:
+jRAPL includes the following methods, and it needs to be declared like the following before usage:
 
 - `public native static int ProfileInit();` Initializes jRAPL. Just needs call once in the beginning.
 
-- `public native static String EnergyStatCheck();`: It returns a string value, which contains the format of content like:
-
-socket1_gpu/dram_energy#socket1_cpu_energy#socket1_package_energy@socket2_gpu/dram_energy#socket2_cpu_energy#socket2_package_energy....
-
-The character '#' to seperate the energy metric information wthin the socket. Character '@' to seperate different socket. If it only has one socket, there is no '@'.
+- `public native static String EnergyStatCheck();` It returns a string value, which contains the format of content like: `socket1_gpu/dram_energy#socket1_cpu_energy#socket1_package_energy@socket2_gpu/dram_energy#socket2_cpu_energy#socket2_package_energy`. The character '#' seperates the energy information per the socket. Character '@' seperates different sockets. If it only has one socket, there is no '@'.
 
 - `public native static void ProfileDealloc();`: Free the memory initialized in `ProfileInit()`.
 
+The `EnergyCheckUtils.java` class provide a working sample on how to use jRAPL.
 
-Known limitations:
+
+Known limitations
 --------------------------------------------------------------
 
 It only works for at most 2 sockets CPU.
